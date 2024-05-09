98c49b2a008100796ae702afae0aff84f6ae4d38
monis-iqbal <monisfunter7@gmail.com>
2024-05-10 01:09:13 +0500

    Guess the number game
    Guess the number game project

---
+++ index.js
@@ -0,0 +1,17 @@
+import inquirer from "inquirer";
+const randomNum = Math.floor(Math.random() * 5);
+console.log("Welcome to Guess the Number game");
+const answer = await inquirer.prompt([
+    {
+        name: "guessednum",
+        type: "number",
+        message: "guess a num"
+    }
+]);
+if (answer.guessednum === randomNum) {
+    console.log("your guess is right");
+}
+else {
+    console.log("wrong guess");
+}
+console.log(randomNum);
